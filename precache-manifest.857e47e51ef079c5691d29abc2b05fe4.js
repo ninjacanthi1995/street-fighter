959aca17172d0bd5a6771c00cd3cd11403bf16c6
Chau HOANG <hoangminhchaucp54@gmail.com>
2021-06-30 14:38:45 +0200

    Updates

---
+++ precache-manifest.857e47e51ef079c5691d29abc2b05fe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2de55fad2d809ad00f98a15cf30f928",
+    "url": "/street-fighter/index.html"
+  },
+  {
+    "revision": "814ce10131ff01b4e8c4",
+    "url": "/street-fighter/static/css/2.60ff825d.chunk.css"
+  },
+  {
+    "revision": "6135643173ed5f1772b0",
+    "url": "/street-fighter/static/css/main.f31b1abb.chunk.css"
+  },
+  {
+    "revision": "814ce10131ff01b4e8c4",
+    "url": "/street-fighter/static/js/2.01e2b523.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/street-fighter/static/js/2.01e2b523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6135643173ed5f1772b0",
+    "url": "/street-fighter/static/js/main.fd9d7285.chunk.js"
+  },
+  {
+    "revision": "fb26459ed9f000796fe7",
+    "url": "/street-fighter/static/js/runtime-main.db5516d8.js"
+  }
+]);
